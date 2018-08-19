e61d7efff09a3d527550bf3b857a38ff238a38a9
softcomet <realseniordev210@gmail.com>
2018-08-19 16:54:40 +0200

    built bizintro architecture

---
+++ src/js/components/Header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Anchor from 'grommet/components/Anchor';
+import Box from 'grommet/components/Box';
+import Header from 'grommet/components/Header';
+import Menu from 'grommet/components/Menu';
+import GrommetIcon from 'grommet/components/icons/base/BrandGrommetOutline';
+import SearchIcon from 'grommet/components/icons/base/Search';
+
+export default function AppHeader (props) {
+  return (
+    <Header justify="center" colorIndex="neutral-4">
+      <Box size={{width: {max: 'xxlarge'}}} direction="row"
+        responsive={false} justify="start" align="center"
+        pad={{horizontal: 'medium'}} flex="grow">
+        <GrommetIcon colorIndex="brand" size="large" />
+        <Box pad="small" />
+        <Menu label="Label" inline={true} direction="row" flex="grow">
+          <Anchor href="/detail">News</Anchor>
+          <Anchor href="/login">Sign In</Anchor>
+          <Anchor href="/login">Signup</Anchor>
+        </Menu>
+
+        <Box flex="grow" align="end">
+          <SearchIcon />
+        </Box>
+      </Box>
+    </Header>
+  );
+};
